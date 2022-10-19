080fc93c94ec3abf51eb17752645e3621bb853f5
alumne <amontero@iesebre.com>
2022-10-19 17:20:39 +0200

    error amb l'anterior commit solucionat

--- app/src/main/java/com/example/counterams/MainActivity.kt
+++ app/src/main/java/com/example/counterams/MainActivity.kt
@@ -2,12 +2,38 @@ package com.example.counterams
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    internal lateinit var tapMeButton: Button
+    internal lateinit var counter: TextView
+    internal lateinit var time: TextView
+    internal var count = 0
+    internal var timer = 60
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        tapMeButton = findViewById(R.id.tapMeButton)
+        counter = findViewById(R.id.counter)
+        time = findViewById(R.id.time)
+        tapMeButton.setOnClickListener {
+            incrementCounter()
+        }
+        time.text=getString(R.string.timeText,timer)
     }
+
+    private fun incrementCounter() {
+
+        count = count + 1
+        counter.text = "comtador: "+count.toString()
+
+
+    }
+
 
 }
