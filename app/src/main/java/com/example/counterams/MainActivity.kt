0903eeecb82b5c345cfb5e744b08f3e852a2135e
alumne <amontero@iesebre.com>
2022-11-09 17:42:59 +0100

    menu

--- app/src/main/java/com/example/counterams/MainActivity.kt
+++ app/src/main/java/com/example/counterams/MainActivity.kt
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 private val INITTAL_TIME =20
@@ -48,6 +51,29 @@ private val INITTAL_TIME =20
 
         }
         time.text = getString(R.string.timeText, timer)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu,menu)
+        return true
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId== R.id.actionAbout){
+            showInfo()
+        }
+        return true
+    }
+
+    private fun showInfo() {
+       val dialogTitle = getString(R.string.aboutTitle,BuildConfig.VERSION_NAME)
+       val dialogMessage = getString(R.string.aboutMessage )
+
+        var builder = AlertDialog.Builder(this)
+        builder.setTitle(dialogTitle).setMessage(dialogMessage).create().show()
+
     }
 
     private fun startGame() {
